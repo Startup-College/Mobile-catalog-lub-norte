b7cfef9b7dec6204da3aa144ca8399ed4b969d3a
Kennedy Ramos <devnedy@gmail.com>
2024-01-20 15:09:45 -0300

    feat(GER-69):Tela home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,23 +1,73 @@
-import { View } from "react-native";
+import { Text, View, TextInput } from "react-native";
 import React from "react";
-import { Container } from "./styles";
-import Button from "../../components/Button/Button";
-import Text from "../../components/Text/Text";
+import Logo from '../../assets/logo_amarelo.svg'
 
-const Home = ({ navigation }) => {
-  function handlePress() {
-    navigation.navigate("Settings");
-  }
+
+
+const header = () => {
   return (
-    <Container>
-      <Text size="LG" weight="BOLD">
-        Home
-      </Text>
-      <View style={{ width: "50%" }}>
-        <Button variant="PRIMARY" title="Settings" onPress={handlePress} />
-      </View>
-    </Container>
+    
+    <View>
+        {/* header */}
+        <View
+        style={{
+            width: '100%',
+            height: 120,
+            padding: 10,
+            backgroundColor: '#144D2A',
+            alignItems: "center",
+            justifyContent: "flex-end"
+        }}
+        >
+    
+
+            <Logo
+                height='80%'
+                svgXmlData={Logo}
+            />
+            
+        </View>
+        {/* fim header */}
+
+        {/* main */}
+        <View   style={{margin: 15}}>
+            <View>
+            <Text
+                style={{
+                    fontSize: 15,
+                    marginBottom: 15,
+                    fontWeight: "bold"
+                }}
+            >Ola, Fulano</Text> 
+
+                <TextInput placeholder="Marca, Viscosidade, Classificação..."
+                style={{
+                    height: 35,
+                    marginVertical: 10,
+                    backgroundColor:'#D9D9D9',
+                    borderRadius: 5,
+                }}
+                />            
+            </View>
+
+            <View>
+
+                <Text
+                    style={{
+                        fontWeight: "bold",
+                        fontSize: 25,
+                        marginVertical: 25,
+                        color:'#144D2A',
+                    }}
+                >Segmentos
+                </Text>
+
+            </View>
+        </View>
+        {/* fim main */}
+
+    </View>
   );
 };
 
-export default Home;
+export default header;
